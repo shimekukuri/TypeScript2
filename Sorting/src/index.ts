a6cdf5066c4593e2fcc6847f3af412de61b408f4
Tyler Hutchinson <jhutchinson192390@gmail.com>
2022-11-29 15:32:25 -0500

    f

--- Sorting/src/index.ts
+++ Sorting/src/index.ts
@@ -1,8 +1,22 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { LinkedList } from './LinkedListCollection';
+import { CharactersCollection } from './CharactersCollection';
 
-const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
-console.log(numbersCollection.data);
-console.log('yolo');
+const numberCollection = new NumbersCollection([-1, -3, -4, 3, 2, 1, 4, 8]);
+numberCollection.sort();
+console.log(numberCollection.data);
+
+const characters = new CharactersCollection('letqepxX');
+characters.sort();
+console.log(characters.data);
+
+const linked = new LinkedList();
+linked.add(1);
+linked.add(-1);
+linked.add(10);
+linked.add(12);
+linked.add(-11);
+linked.sort();
+
+linked.print();
