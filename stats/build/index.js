2f818f2d6bb34a257ffd52bdac5499082bb0fe88
Tyler Hutchinson <jhutchinson192390@gmail.com>
2022-11-30 15:05:48 -0500

    -

--- stats/build/index.js
+++ stats/build/index.js
@@ -4,6 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = __importDefault(require("fs"));
+const startTime = performance.now();
 const matches = fs_1.default
     .readFileSync('football.csv', {
     encoding: 'utf-8',
@@ -12,4 +13,28 @@ const matches = fs_1.default
     .map((row) => {
     return row.split(',');
 });
-console.log(matches);
+let manUnitedWins = 0;
+var MatchResult;
+(function (MatchResult) {
+    MatchResult["HomeWin"] = "H";
+    MatchResult["AwayWin"] = "A";
+    MatchResult["Draw"] = "D";
+})(MatchResult || (MatchResult = {}));
+const printMatchResult = () => {
+    if (match[5] === 'H') {
+        return MatchResult.HomeWin;
+    }
+    return MatchResult.AwayWin;
+};
+for (let match of matches) {
+    if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+        manUnitedWins++;
+    }
+    else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+        manUnitedWins++;
+    }
+}
+const endTime = performance.now();
+console.log(`Exectued in ${endTime - startTime} ms`);
+console.log(manUnitedWins);
+console.log('end');
