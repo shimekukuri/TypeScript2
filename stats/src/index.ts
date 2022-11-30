4a25843a92924b476aaffc7b1d4f8a174a6209e9
Tyler Hutchinson <jhutchinson192390@gmail.com>
2022-11-30 15:05:57 -0500

    -

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,5 +1,7 @@
 import fs from 'fs';
 
+const startTime = performance.now();
+
 const matches = fs
   .readFileSync('football.csv', {
     encoding: 'utf-8',
@@ -9,4 +11,23 @@ const matches = fs
     return row.split(',');
   });
 
-console.log(matches);
+let manUnitedWins = 0;
+
+enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D',
+}
+
+for (let match of matches) {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+    manUnitedWins++;
+  }
+}
+
+const endTime = performance.now();
+console.log(`Exectued in ${endTime - startTime} ms`);
+console.log(manUnitedWins);
+console.log('end');
