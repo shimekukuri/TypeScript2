dc60506c478e226484fcbef85731bf1b885f1529
Tyler Hutchinson <jhutchinson192390@gmail.com>
2022-12-16 15:47:25 -0500

    -

--- server/src/controllers/decorators/controller.ts
+++ server/src/controllers/decorators/controller.ts
@@ -1,16 +1,21 @@
 import 'reflect-metadata';
-import express from 'express';
-
-export const router = express.Router();
+import { AppRouter } from '../../appRouter';
+import { Methods } from './Methods';
 
 export function controller(routePrefix: string) {
   return function (target: Function) {
+    const router = AppRouter.getInstance();
     for (let key of Object.getOwnPropertyNames(target.prototype)) {
       const routeHandler = target.prototype[key];
       const path = Reflect.getMetadata('path', target.prototype, key);
+      const method: Methods = Reflect.getMetadata(
+        'method',
+        target.prototype,
+        key
+      );
 
       if (path) {
-        router.get(`${routePrefix}${path}`, routeHandler);
+        router[method](`${routePrefix}${path}`, routeHandler);
       }
     }
   };
