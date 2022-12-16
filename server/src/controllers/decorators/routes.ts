8c292788bfdf52a806f553c7fcceb18deabf452c
Tyler Hutchinson <jhutchinson192390@gmail.com>
2022-12-16 15:47:37 -0500

    -

--- server/src/controllers/decorators/routes.ts
+++ server/src/controllers/decorators/routes.ts
@@ -1,8 +1,17 @@
 import 'reflect-metadata';
+import { Methods } from './Methods';
 
-export function get(path: string) {
-  return function (target: any, key: string, desc: PropertyDescriptor) {
-    Reflect.defineMetadata('path', path, target, key);
-    console.log(Reflect.getMetadata('path', target, key));
+function routerBinder(method: string) {
+  return function (path: string) {
+    return function (target: any, key: string, desc: PropertyDescriptor) {
+      Reflect.defineMetadata('path', path, target, key);
+      Reflect.defineMetadata('method', method, target, key);
+    };
   };
 }
+
+export const get = routerBinder(Methods.get);
+export const put = routerBinder(Methods.put);
+export const post = routerBinder(Methods.post);
+export const del = routerBinder(Methods.del);
+export const patch = routerBinder(Methods.patch);
